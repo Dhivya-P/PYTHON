7afb6a8bd38ff6a26849ea15b10757393cf16c1c
Dhivya-P <102724567+Dhivya-P@users.noreply.github.com>
2022-03-31 21:42:30 +0530

    Create Positive.py

---
+++ COLLECTONS/Positive.py
@@ -0,0 +1,21 @@
+def numbers(num):
+  pos= 0
+  neg=0
+  for j in num:
+      if(j>0):
+        pos=pos+1
+      else:
+        neg=neg+1
+  print("Number of positive numbers:",pos)
+  print("Number of negative number:",neg)
+numbers(list)
+		while(product>0):
+			rem=product%10
+			sum=sum+rem
+			product=product//10
+		print("sum of their product is",sum)
+	else:
+		print("product is:",product)
+n1=int(input("enter n1 value:"))
+n2=int(input("enter n2 value:"))
+product_or_sum(n1,n2)
