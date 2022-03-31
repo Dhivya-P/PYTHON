529602dfac74e0f28c3577e742bbe916fb4f0fb4
Dhivya-P <102724567+Dhivya-P@users.noreply.github.com>
2022-03-31 21:39:54 +0530

    Create Prime.py

---
+++ COLLECTONS/Prime.py
@@ -0,0 +1,12 @@
+def prime_checker(n):
+  c=0
+  if n!=1:
+    for i in range(1,n+1):
+      if(n%i==0):
+        c=c+1
+  if(c==2):
+    print(n,"is a prime number")
+  else:
+    print(n,"is not a prime number")
+n=int(input("enter a number to check whether it is a prime number or not:"))
+prime_checker(n)
